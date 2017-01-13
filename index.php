97475f08ce1fc15ee32d73eac85a9074bc03464c
Antoinfo <new@mail.com>
2017-01-13 14:27:54 +0100

    index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+ 
+   <head>
+      <meta charset="UTF-8"/>
+      <meta name="author" content="Nom de l'auteur"/>
+      <meta name="keywords" content="Mots clefs relatifs à cette page"/>
+      <meta name="description" content="Descriptif court"/>
+      <title>Titre de ma page</title>
+   </head>
+ 
+   <body>
+      <h1>Ma première page web</h1>
+   </body>
+</html>
